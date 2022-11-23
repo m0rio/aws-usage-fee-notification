14b41e74729b6b527e81ef26d17654e688be849a
m0rio <morionzeni@gmail.com>
2022-11-23 17:49:36 +0900

    EventBridgeから起動するように修正

--- lambda_function.py
+++ lambda_function.py
@@ -12,6 +12,27 @@ ce_client = boto3.client('ce')
 channel_access_token = os.getenv('LINE_CHANNEL_ACCESS_TOKEN', None)
 line_bot_api = LineBotApi(channel_access_token)
 
+user_id = os.getenv('USER_ID')
+
+# Main
+def lambda_handler(event, context):
+
+    # 月初から今日までのAWS利用料金の取得
+    today = datetime.today()
+    first_day = today.replace(day=1)
+
+    # Line送信用メッセージの作成
+    message = get_aws_cost(first_day, today)
+
+    # Lineにメッセージ送信
+    message_obj = TextSendMessage(text=message)
+    line_bot_api.push_message(user_id, message_obj)
+
+    return {
+        'statusCode': 200,
+        'body': ''
+    }
+
 
 # 指定した期間のAWS利用料を返す関数
 def get_aws_cost(start_day, end_day):
@@ -46,79 +67,3 @@ def create_message(start_day, end_day, aws_cost):
     message = '開始日: ' + start_day.strftime('%Y年%m月%d日') + '\n' + '終了日: ' + end_day.strftime(
         '%Y年%m月%d日') + '\n' + 'AWS利用料: $' + str(round(float(aws_cost), 1))
     return message
-
-
-# Main
-def lambda_handler(event, context):
-    body = json.loads(event['body'])
-    print(body)
-
-    # Webhookの接続確認用
-    if len(body['events']) == 0:
-        return {
-            'statusCode': 200,
-            'body': ''
-        }
-
-    # (メッセージを返答するときにはreply_tokenを使ってリプライ用のAPIを叩く)
-    args = body['events'][0]['message']['text'].split(" ")
-    reply_token = body['events'][0]['replyToken']
-
-    # 引数チェック
-    if len(args) == 2:
-        try:
-            check_date_format = args[0] + args[1]
-            datetime.strptime(check_date_format, '%Y%m')
-            year = args[0]
-            month = args[1]
-        except ValueError:
-            message = '[Error]' + '\n' + \
-                'Incorrect data format, should be YYYY MM'
-            message_obj = TextSendMessage(text=message)
-            line_bot_api.reply_message(reply_token, message_obj)
-            raise ValueError(message)
-    else:
-        year = None
-        month = None
-
-    # 変数YearとMonthがNullでなければ、指定した年月の月額利用料を取得する
-    # Nullの場合は、月初から本日までの利用料を取得する
-    if all(v is not None for v in [year, month]):
-
-        # 指定した年月の月額利用料を取得する
-        year_int = int(year)
-        month_int = int(month)
-
-        first_last_day = get_frst_last_date(year_int, month_int)
-        start_day = date(year_int, month_int, 1)
-        end_day = date(year_int, month_int, first_last_day[1])
-
-        # Line送信用メッセージの作成
-        # (コスト計算とメッセージ作成はセットなのでまとめたほうがいいかと)
-        message = get_aws_cost(start_day, end_day)
-
-        # Lineにメッセージ返信
-        # (ライブラリのメソッド名を関数名にするのは混乱するのでメイン関数に処理を持ってきた)
-        message_obj = TextSendMessage(text=message)
-        line_bot_api.reply_message(reply_token, message_obj)
-
-        return {
-            'statusCode': 200,
-            'body': ''
-        }
-
-    # 月初から今日までのAWS利用料金の取得
-    today = datetime.today()
-    first_day = today.replace(day=1)
-
-    # Line送信用メッセージの作成
-    message = get_aws_cost(first_day, today)
-
-    # Lineにメッセージ送信
-    message_obj = TextSendMessage(text=message)
-    line_bot_api.reply_message(reply_token, message_obj)
-
-    return {
-        'statusCode': 200,
-        'body': ''
-    }
